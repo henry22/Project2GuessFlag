9c4a5a2269bbe648ea5cb22db229c1fc5c520e7d
henry22 <henry811010@gmail.com>
2015-05-02 11:58:36 +0800

    Correct the name of third button and add the function askQuestion.

--- Project2GuessFlag/ViewController.swift
+++ Project2GuessFlag/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var secondButton: UIButton!
     
-    @IBOutlet weak var thridButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
     
     var countries = [String]()
     var score = 0
@@ -24,6 +24,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        //UIImage(named:) read from an array by position
+        //firstbutton.setImage() assigns a UIImage to the button
+        //forState: .Normal The setImage() method which state of the button should be changed?which means "the standard state of the button."
+        //.Normal it understands that to mean "the Normal value of UIControlState."
+        firstButton.setImage(UIImage(named: countries[0]), forState: UIControlState.Normal)
+        secondButton.setImage(UIImage(named: countries[1]), forState: UIControlState.Normal)
+        thirdButton.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
